13c9772a5df02b8e773659ba38ea07af936b280c
UNIVN <valuable.network.lz@gmail.com>
2025-03-27 17:21:42 +0800

    add str tools: to uint64

--- strs/strs.go
+++ strs/strs.go
@@ -16,3 +16,11 @@ func ToUint64(s string) uint64 {
 	}
 	return value
 }
+
+func ToInt64(s string) int64 {
+	value, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		return 0
+	}
+	return value
+}
